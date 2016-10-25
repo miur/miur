dedd39188ba534706ac6c8c1e96b035b49d3e991
Amerlyq <amerlyq@gmail.com>
2016-10-25 21:53:43 +0300

    [proto] +

--- miur.py
+++ miur.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python
 
+import os
 import curses
+
+
+entries = os.listdir(os.getcwd())
 
 
 def main(stdscr):
@@ -12,6 +16,10 @@ def main(stdscr):
     height = 5
     width = 40
     win = curses.newwin(height, width, begin_y, begin_x)
+
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    for i, e in enumerate(entries):
+        stdscr.addstr(i, 0, e, curses.color_pair(1))
 
     stdscr.refresh()
     stdscr.getkey()
