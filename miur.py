4aceb68a9f3e0261ec5b7f17f20250b4f64e2ec8
Amerlyq <amerlyq@gmail.com>
2016-11-06 19:24:02 +0200

    [proto] rfc main

--- miur.py
+++ miur.py
@@ -1,44 +1,53 @@
 #!/usr/bin/python
 
-import time
 import logging
 import multiprocessing as mp
 
-from miur.cursor import main as curs
-from miur.ui import tui
-# from miur.relay.server import Listener
-from miur.relay.aux import send
 
-logging.basicConfig(filename='/tmp/miur.log', level=logging.DEBUG)
+server_address = ('127.0.0.1', 8888)
 
 
-def test(server_address):
+# DEV: redirect stderr to logging
+# SEE http://stackoverflow.com/questions/7714868/python-multiprocessing-how-can-i-reliably-redirect-stdout-from-a-child-process
+def cursor():
+    from miur.cursor import main as curs
+    try:
+        curs.main(server_address)
+    except KeyboardInterrupt:
+        pass
+
+
+def test():
+    import time
+    from miur.relay.aux import send
     try:
         time.sleep(1.0)
-        send(server_address, 'hi')
+        send(server_address, 'test')
     except KeyboardInterrupt:
         pass
 
 
 if __name__ == '__main__':
-    # BAD: not deleted afterwards
-    # with Listener('/tmp/miur') as l:
-    # with Listener(("localhost", 7773)) as l:
-    #     tui.main(l.server.server_address)
+    logging.basicConfig(level=logging.INFO,
+                        filename='/tmp/miur.log',
+                        datefmt='%H:%M:%S',
+                        format="%(asctime)s %(levelname)s " +
+                        "[%(module)s:%(lineno)d] %(message)s")
+    logging.info('*' * 50)
 
     # mp.set_start_method('spawn')
-
     # exit_event = mp.Event()
-    server_address = ('127.0.0.1', 8888)
-    # DEV: redirect stderr to logging
-    # SEE http://stackoverflow.com/questions/7714868/python-multiprocessing-how-can-i-reliably-redirect-stdout-from-a-child-process
-    p_curs = mp.Process(target=curs.main, args=(server_address,))
-    p_test = mp.Process(target=test, args=(server_address,))
+
+    # EXPL: pass args to process
+    # p_curs = mp.Process(target=cursor, args=(server_address,))
+    p_curs = mp.Process(target=cursor)
+    p_test = mp.Process(target=test)
 
     p_curs.start()
     p_test.start()
 
     try:
+        from miur.ui import tui
         tui.main(None)
     except KeyboardInterrupt:
         pass
