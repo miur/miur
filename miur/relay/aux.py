d408a8c9c2e8d5e5cedcadded2add058a279ae03
Amerlyq <amerlyq@gmail.com>
2016-11-07 02:55:12 +0200

    [proto] queue bus to servers

--- miur/relay/aux.py
+++ miur/relay/aux.py
@@ -7,8 +7,11 @@ from miur.relay import protocol
 
 _log = logging.getLogger(__name__)
 
+is_watching = True
 _servers = {}  # task -> (reader, writer)
 active_srv = None
+qcmd = asyncio.Queue()
+qdat = asyncio.Queue()
 
 
 def send_once(server_address, obj):
@@ -45,14 +48,12 @@ def send_once(server_address, obj):
 #       => cached dom/frame must be saved independently ?
 
 async def core_connect(server_address, loop):
-    global _servers
     reader, writer = await asyncio.open_connection(*server_address, loop=loop)
     _servers[server_address] = (reader, writer)
     _log.info('Connected to {!r}'.format(server_address))
 
 
 async def core_send(obj):
-    global _servers, active_srv
     (_, writer) = _servers[active_srv]
     _log.info('Sent: {!r}'.format(obj))
     data = protocol.serialize(obj)
@@ -61,29 +62,72 @@ async def core_send(obj):
 
 
 async def core_recv():
-    global _servers, active_srv
     (reader, _) = _servers[active_srv]
     # data = await asyncio.wait_for(reader.readline(), timeout=2.0)
-    data = await reader.readline()
+    data = await reader.read()
     obj, _ = protocol.deserialize(data)
     _log.info('Recv: {!r}'.format(obj))
     return obj
 
 
 async def core_close():
-    global _servers, active_srv
     (_, writer) = _servers[active_srv]
     writer.close()
     del _servers[active_srv]
     _log.info('Disconnected from {!r}'.format(active_srv))
 
 
+async def qcmd_send():
+    while is_watching:
+        cmd = await qcmd.get()
+        await core_send(cmd)
+
+
+async def qdat_recv():
+    while is_watching:
+        obj = await core_recv()
+        await qdat.put(obj)
+        _log.debug('Size qdat: {!r}'.format(qdat.qsize()))
+
+
+async def qdat_apply():
+    while is_watching:
+        obj = await qdat.get()
+        print(obj)
+
+
+def put_cmd(obj):
+    qcmd.put_nowait(obj)
+    _log.debug('Size qcmd: {!r}'.format(qcmd.qsize()))
+
+
+async def test():
+    global is_watching
+
+    await asyncio.sleep(0.5)
+    put_cmd('yes\n')
+    await asyncio.sleep(0.5)
+    is_watching = False
+    await asyncio.sleep(0.5)
+    # BAD: need one more outgoing msg to exit all loops
+    # BAD: qdat recv won't exit until response (which can never come)
+    put_cmd('end\n')
+    await asyncio.sleep(0.5)
+    put_cmd('bad\n')
+
+
 def loop(server_address):
     global active_srv
+
     active_srv = server_address  # OR another id
 
     loop = asyncio.get_event_loop()
     loop.run_until_complete(core_connect(server_address, loop))
-    loop.run_until_complete(core_send('yes\n'))
+    tasks = [loop.create_task(qcmd_send()),
+             loop.create_task(qdat_recv()),
+             loop.create_task(qdat_apply()),
+             loop.create_task(test())]
+    loop.run_until_complete(asyncio.gather(*tasks))
     loop.run_until_complete(core_close())
+
     loop.close()
