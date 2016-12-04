f612ed84640d6e0be25e6585fea022682c9ae7fc
Amerlyq <amerlyq@gmail.com>
2016-12-04 20:47:57 +0200

    [srv] Temp: conn list with broken locks

--- miur/core/main.py
+++ miur/core/main.py
@@ -2,49 +2,52 @@ import logging
 import asyncio
 
 from .executor import executor
-from .server import ClientProtocol, sender
+from . import server
 
 _log = logging.getLogger(__name__)
 
 
 # FIXME: rm 'Bay' altogether -- *core* may have no server at all when tight linking
+#   * ALSO: there may be two sep srv for TCP/UDP simultaneously
 # BUT: I used 'with Bay' instead of using 'try: ... finally: ...' for clean-up
 class Bay:
     def __init__(self, server_address, loop):
         self.server_address = server_address
         self.loop = loop
+        self.conn = server.ClientConnections()
 
     # TRY: __aenter__ and __aexit__
     def __enter__(self):
         # Each client connection will create a new protocol instance
         # import functools
         # functools.partial(ClientProtocol, loop, callback),
-        # SEE self.server.sockets -- to manipulate raw socket
+        # SEE self.srv.sockets -- to manipulate raw socket
 
         # FIX:DEV: await on create_server -- instead of 'run_until_complete(coro)'
         #   => because there may be no server at all for tight coupling (using run_forever())
-        coro = self.loop.create_server(lambda: ClientProtocol(self.loop),
-                                       *self.server_address,
-                                       reuse_address=True, reuse_port=True)
-        self.server = self.loop.run_until_complete(coro)
-        _log.info('Serving on {}'.format(self.server.sockets[0].getsockname()))
+        coro = self.loop.create_server(
+            lambda: server.ClientProtocol(self.loop, self.conn),
+            *self.server_address, reuse_address=True, reuse_port=True)
+        self.srv = self.loop.run_until_complete(coro)
+        _log.info('Serving on {}'.format(self.srv.sockets[0].getsockname()))
+        return self.conn
 
     def __exit__(self, *args):
-        ClientProtocol.disconnectAll()
+        self.conn.disconnectAll()
         # CHECK: earlier loop.stop() in do_quit() must not obstruct this ops
-        self.server.close()
-        self.loop.run_until_complete(self.server.wait_closed())
+        self.srv.close()
+        self.loop.run_until_complete(self.srv.wait_closed())
 
 
 def main_loop(server_address):
     loop = asyncio.get_event_loop()
     loop.set_debug(True)
 
-    with Bay(server_address, loop):
+    with Bay(server_address, loop) as conn:
         # FIXME:RFC task is independent from Bay
         #   => server can reject new conn but continue to serve already established
         loop.create_task(executor())
-        loop.create_task(sender(ClientProtocol.send))
+        loop.create_task(server.sender(conn))
         loop.run_forever()
 
     loop.close()
--- miur/core/server.py
+++ miur/core/server.py
@@ -8,65 +8,88 @@ from miur.share import protocol
 _log = logging.getLogger(__name__)
 
 
-class ClientProtocol(asyncio.Protocol):
-    _clients = {}
-    _lock = threading.Lock()
+# NOTE: mixing ClientProtocol with static vars is BAD idea
+# BETTER: re-impl loop.create_server(...) for deterministic order of accept/receive/lost
+#   => then parallelism will be controllable and broken lock may become unnecessary
+# NOTE: list of connections must be sep entity
+#   << if you create two separate servers -- one for TCP and another for UDP
+#       -- to support two diff clients lists
+class ClientConnections(dict):
+    def __init__(self):
+        self._lock = threading.Lock()
 
-    def __init__(self, loop):
+    def __getitem__(self, cid):
+        # THINK? items have refcount so calling their methods is threadsafe after getitem?
+        with self._lock:
+            return super().__getitem__(cid)
+
+    def __setitem__(self, cid, obj):
+        with self._lock:
+            return super().__setitem__(cid, obj)
+
+    def __delitem__(self, cid):
+        # BAD: crash if waits on _lock until disconnectAll() exits
+        with self._lock:
+            _log.info('Client {} removed'.format(cid))
+            return super().__delitem__(cid)
+
+    # ALT: overload generator :: [c.close() for c in conn.values()]
+    def disconnectAll(self):
+        # BAD: won't close any connection pending on _lock to in __setitem__
+        with self._lock:
+            for cid, obj in self.items():
+                _log.info('Closing the client {!r}'.format(cid))
+                obj.close()
+
+
+class ClientProtocol(asyncio.Protocol):
+    """One instance of this class is created for each connected client"""
+
+    def __init__(self, loop, conn):
         self.loop = loop
+        self.conn = conn
         self.is_processing = True
 
     def connection_made(self, transport):
         self.transport = transport
         self.peer = self.transport.get_extra_info('peername')
         _log.info('Connection from {}'.format(self.peer))
-        self.cid = self.peer  # ALT: use first msg from client as its cid
-        with ClientProtocol._lock:
-            ClientProtocol._clients[self.cid] = self
+        # ALT: use first msg from client as its cid
+        # NOTE: adding to dict don't require lock (beside iterating that dict)
+        self.conn[self.peer] = self
 
     def connection_lost(self, exc):
         _log.info('Connection lost {}'.format(self.peer))
         if exc is not None:
             raise exc
-        with ClientProtocol._lock:
-            # BAD: crash if waits on _lock until disconnectAll() exits
-            del ClientProtocol._clients[self.cid]
+        del self.conn[self.peer]
+
+    def close(self):
+        _log.info('Closing the client {!r}'.format(self.peer))
+        self.transport.close()
 
     def data_received(self, data):
         # NEED:DEV: loop for long data
         obj, ifmt = protocol.deserialize(data)
-        _log.info('Recv:{!r}: {!r}'.format(self.cid, obj))
+        _log.info('Recv:{!r}: {!r}'.format(self.peer, obj))
 
         if self.is_processing is True:
             if obj.get('cmd', '') == 'quit-all':
                 self.loop.create_task(bus.do_quit(self.loop))
                 self.is_processing = False
-            bus.qin.put_nowait((self.cid, (ifmt, obj)))
+            bus.qin.put_nowait((self.peer, (ifmt, obj)))
 
-    def send_data(self, obj, ofmt=None):
-        _log.info('Send:{!r}: {!r}'.format(self.cid, obj))
+    def send(self, obj, ofmt=None):
+        _log.info('Send:{!r}: {!r}'.format(self.peer, obj))
         data = protocol.serialize(obj, ofmt)
         # BAD: exc if client was already deleted when executor was suspended
         self.transport.write(data)
-
-    @staticmethod
-    async def send(cid, obj, ofmt=None):
-        # NOTE: can't await send_data w/o create_task
-        with ClientProtocol._lock:
-            return ClientProtocol._clients[cid].send_data(obj, ofmt)
-
-    @staticmethod
-    def disconnectAll():
-        # BAD: won't close any connection, waiting on _lock to add
-        with ClientProtocol._lock:
-            for client in ClientProtocol._clients.values():
-                _log.info('Closing the client {!r} socket'.format(client))
-                client.transport.close()
+        # CHECK: if need '.drain()' ::: need only for streams
 
 
-async def sender(send_rsp):
+async def sender(conn):
     while True:
-        cid, (ifmt, rsp) = await bus.qout.get()
+        cid, (ofmt, rsp) = await bus.qout.get()
         _log.debug('Response to: {!r}'.format(rsp['id']))
-        await send_rsp(cid, rsp, ifmt)
+        conn[cid].send(rsp, ofmt)
         bus.qout.task_done()
