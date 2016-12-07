7ca55a731bef1936a87f5b3027533b4747629f9d
Amerlyq <amerlyq@gmail.com>
2016-12-07 03:27:03 +0200

    [doc] accessors sep

--- miur/core/fs.py
+++ miur/core/fs.py
@@ -2,6 +2,14 @@
 
 import os
 
+# IDEA: Access to accessors must be sync (blocking), and thread pool only at core.
+# - BAD each long op will block any next short ones
+# - for flexibility it would be better for accessors to accept cmds as input instead of callbacks.
+#   => then I can reuse all other bus facilities
+# - what if remote uses only accessor and core is placed at host?
+#   => then thread pool at remote will provide nice efficiency
+#   ! in such case I need server attached to accessor anyways !
+
 
 # Can't be specified for general non-tree graphs
 # There you must track your whole path and move back to last adjacent node
