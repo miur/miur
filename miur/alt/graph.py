54c5d3dee50980088bae03150c8bba48aa61562f
Amerlyq <amerlyq@gmail.com>
2017-10-11 02:07:24 +0300

    [graph] inner structs thoughts

--- miur/alt/graph.py
+++ miur/alt/graph.py
@@ -6,8 +6,13 @@ _log = logging.getLogger(__name__.split('.', 2)[1])
 # https://stackoverflow.com/questions/19472530/representing-graphs-data-structure-in-python
 #   http://igraph.org/
 #   http://networkx.github.io/
-#   TRY:USE: https://networkx.github.io/documentation/stable/tutorial.html
+#   TRY:USE: https://networkx.github.io/documentation/latest/reference/classes/digraph.html#methods
 #     => use complete graph library to understand necessary API and limitations
+#   THINK: embed to miur to provide algorithms on graphs
+#     * bake current complete fs state -- load necessary meta for all files
+#     * apply alg to networkx graph imported from native baked graph
+#     * mark some parts of graph as dirty/out-of-date by inotifywait events
+#     * lazy reload dirty parts only when they appear in viewport
 
 # ++++ https://en.wikipedia.org/wiki/Graph_rewriting
 # https://en.wikipedia.org/wiki/Graph_(abstract_data_type)#Representations
@@ -17,7 +22,7 @@ _log = logging.getLogger(__name__.split('.', 2)[1])
 
 
 def run(argv):
-    g = GraphContainer()
+    g = RootedGraph()
     n = g.getRoot()
     # NOTE: don't return edges container directly -- wrap in iterator
     #   <= because list may change any time
@@ -68,8 +73,6 @@ class IEdge(object):
 #   * GraphNode is monolith -- you can't acquire access to subnodes from here
 #   * GraphNode connects external edges with some internal input nodes of graph
 #   * GraphNode is membrane separating outer and inner structs
-class IGraph(INode):
-    pass
 
 
 # MAYBE: container inherited from GraphProvider
@@ -80,16 +83,94 @@ class IGraph(INode):
 #   = bus / pipe -- mirror some piece of data from one end to another
 #   + modify/convert content when transferring
 #   + EdgeGraph -- encapsulates whole modification logic
-class GraphContainer(object):
+
+# Both any node or edge could be replaced by graph
+#   => single uid counter <= no need to separate ID-space for nodes and edges
+#   => BUT: separate containers for nodes and edges for enumeration optimizations
+class BaseGraph(object):
     # nodes + attrs
     # edges + attrs
+    def __init__(self):
+        # BAD: separate containers
+        #   * excessive lookups per each hashmap
+        #   * distance in memory -> cache miss
+        #   * must be optimized for graphs which are "mostly tree" (at least)
+        #     => nr_edges ~>= nr_nodes - 1
+        #     => traversing nodes one-by-one => need adjacency info
+        self._nodes = {}
+        self._edges = {}
+        self._neighbors = {}  # cached adjacency
+        self._maxuid = 0
+
+    def _new_uid(self):
+        self._maxuid += 1
+        return self._maxuid
+
+    def add_node(self, obj):
+        uid = self._new_uid()
+        self._nodes[uid] = obj
+        self._neighbors[uid] = set()
+        return uid
+
+    def add_edge(self, uid1, uid2):
+        uid = self._new_uid()
+        # BAD: custom choosen data struct
+        #   => may discard _edges alltogether and keep only _neighbors
+        self._edges[uid] = (uid1, uid2)
+        # BAD: directly saves neighbor uid instead of intermediate 'edge'
+        #   => must save ptrs to 'edge' to be able to be replaced by 'graph'
+        # BAD: how to connect each neighbor with its own point in GraphNode ?
+        #   => only GraphNode may decide it depending on who edge points to
+        #     -> [_] DEV pass neighbor node directly into add_edge() of GraphNode
+        #   => THINK: passing edge is not exclusive -> you still need _neighbors
+        #     when replacing SimpleNode by GraphNode and back to re-supply whole
+        #     list of _neighbors to constructor
+        #     ALSO you need full-fledged object of Edge -- to remain connected
+        #     on one side (surrounding graph knowledge) when replacing edges
+        #     connect-point to another node
+        self._neighbors[uid1].add(uid2)
+        self._neighbors[uid2].add(uid1)
+        # NEED: cache added node in adjacency set of each node
+        return uid
+
     def __str__(self):
         return "graph"
 
+    # Prefer nodes for default operations
+    def __iter__(self):
+        return iter(self._nodes)
+
+    def __getitem__(self, uid):
+        return self._nodes[uid]
+
+    def __contains__(self, uid):
+        return uid in self._nodes
+
+
+# API accepts id (instead of objects) and returns objects itself
+class IdGraph(BaseGraph):
+    pass
+
+
+# API accepts/returns proxy instance of accessors to IdGraph
+#   * each proxy aggregates _uid and _g to be able to operate on itself
+#   == weak_ptr == actual object inside Graph may be destroyed any time
+class ProxyGraph(object):
+    def __init__(self):
+        self._g = IdGraph()
+
+    def get_node(self):
+        return INode(self._g)
+
+
+# Wrapper classes extend base container by shortcuts
+#   E.G. add_node_connected_to(node, [neighbour_nodes])
+#     == add_node() + add_edges_from()
+class RootedGraph(ProxyGraph):
+    def __init__(self, rootid=None, **kw):
+        self._rootid = rootid
+        super().__init__(**kw)
+
     def getRoot(self):
         """ Construct node object with backref to graph """
-        return INode(self)
-
-
-class GraphProvider(IGraph):
-    pass
+        return self.get_node(self.rootid)
