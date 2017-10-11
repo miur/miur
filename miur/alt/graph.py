2693eed1d1b87deb8108650e3935c0ba13c55f0a
Amerlyq <amerlyq@gmail.com>
2017-10-12 02:13:39 +0300

    [graph] split graph into objects and network

--- miur/alt/graph.py
+++ miur/alt/graph.py
@@ -22,12 +22,17 @@ _log = logging.getLogger(__name__.split('.', 2)[1])
 
 
 def run(argv):
-    g = RootedGraph()
-    n = g.getRoot()
+    g = BaseGraph()
+    g.add_node(1)
+    g.add_node(2)
+    g.add_node(3)
+    for n in g:
+        print(n)
+    # n = g.getRoot()
     # NOTE: don't return edges container directly -- wrap in iterator
     #   <= because list may change any time
-    for e in n.getEdges():
-        print(e)
+    # for e in n.getEdges():
+    #     print(e)
 
 # graph = node >> embeddable -- separate containers
 # graph ifc :: shared for containers and evaluators
@@ -36,10 +41,30 @@ def run(argv):
 # object/provider + accessor/adapter = service
 # IDEA: construct Graph from NodeCompositeImpl -- which can contain any type
 #   => substitute impl with NONE / Node / CachedNode / GraphNode unnoticed
+#   => NEED: GraphNode.connectRoot(e) for e in edges
+#       => to specify root (entry) point into graph from each edge viewpoint
+# NOTE: substitute node keeping edges intact by change_node(uid, obj)
+
+
+# NOTE: addr of each node is pair (graph, uid)
+#   ALT: uid unique through all graphs for cross-references
+def g_new_uid(self):
+    g_new_uid._maxuid += 1
+    return g_new_uid._maxuid
+
+
+g_new_uid._maxuid = 0
+
+
+## NOTE: use only if {_edges} will be amnested
+# def g_new_euid(self):
+#     g_new_euid._maxuid += 1
+#     return g_new_euid._maxuid
+# g_new_euid._maxuid = 0
 
 
 # BAD: hard to determine necessary members
-#   ~ dynamic map of attrs -- use protocol to query, list and aquire values
+#   ~ dynamic map of attrs -- use protocol to query, list and acquire values
 #       ? where I need such reflection ?
 #   ~ hardcoded Node classes -- types are checked by python
 # WARN: all Node/Edge must be invalidated when graph deleted
@@ -47,15 +72,12 @@ def run(argv):
 #   -- because they can acquire info from any backref data struct (self._g)
 class INode(object):
     # attributes = {}  # NOTE: edges as one of attributes
-    def __init__(self, g):
+    def __init__(self, g, uid):
         self._g = g
-        self._edges = [IEdge(self._g), IEdge(self._g)]  # TEMP:REM
+        self._uid = uid
 
     def __str__(self):
         return "node"
-
-    def getEdges(self):
-        return iter(self._edges)
 
 
 class IEdge(object):
@@ -87,6 +109,11 @@ class IEdge(object):
 # Both any node or edge could be replaced by graph
 #   => single uid counter <= no need to separate ID-space for nodes and edges
 #   => BUT: separate containers for nodes and edges for enumeration optimizations
+
+# NOTE: adding node, its associated value and edge between nodes -- all are independent separate operations
+#   * layer of graph / relations
+#   * layer of object values
+#   * layer of proxy accessors to virtual/generated graphs and values
 class BaseGraph(object):
     # nodes + attrs
     # edges + attrs
@@ -98,24 +125,43 @@ class BaseGraph(object):
         #     => nr_edges ~>= nr_nodes - 1
         #     => traversing nodes one-by-one => need adjacency info
         self._nodes = {}
-        self._edges = {}
         self._neighbors = {}  # cached adjacency
-        self._maxuid = 0
+        ## NEED: for edges pointing to non-existent uids
+        #   ALSO: traversing/filtering list of all nodes
+        #       -- currently not in priority
+        # self._edges = {}
 
-    def _new_uid(self):
-        self._maxuid += 1
-        return self._maxuid
+        # THINK: is there need for sep _attrs field ?
+        #   * general/common graph-related attrs can be stored directly in graph
+        #       independent from node/edge object in graph itself
+        #   => has sense if graph embodies network/media of some kind
 
-    def add_node(self, obj):
-        uid = self._new_uid()
+    def add_node(self, uid):
+        if uid in self._nodes:
+            raise
+        self._nodes[uid] = None
+        self._neighbors[uid] = set()  # USE: = .setdefault(uid, set())
+        # NOTE: if there are dangling edges which pointed to this uid
+        #   => find them and insert into _neighbors
+        # BET? prohibit dangling edges => easier ALG, lesser overhead
+
+    def set_node_value(self, uid, obj):
         self._nodes[uid] = obj
-        self._neighbors[uid] = set()
+
+    # high-level api -- relocate in its own ObjectGraph superstructure
+    def add_object(self, obj):
+        uid = g_new_uid()
+        self.add_node(uid)
+        self.set_node_value(uid, obj)
         return uid
 
     def add_edge(self, uid1, uid2):
         uid = self._new_uid()
-        # BAD: custom choosen data struct
-        #   => may discard _edges alltogether and keep only _neighbors
+        # BAD: custom chosen data struct
+        #   => may discard _edges altogether and keep only _neighbors
+        #   BUT: you can generate whole node / graph on the other side simply by
+        #   accessing dangling (lazy) edge -- you don't know where you point to
+        #   until generation succeeds
         self._edges[uid] = (uid1, uid2)
         # BAD: directly saves neighbor uid instead of intermediate 'edge'
         #   => must save ptrs to 'edge' to be able to be replaced by 'graph'
@@ -125,7 +171,7 @@ class BaseGraph(object):
         #   => THINK: passing edge is not exclusive -> you still need _neighbors
         #     when replacing SimpleNode by GraphNode and back to re-supply whole
         #     list of _neighbors to constructor
-        #     ALSO you need full-fledged object of Edge -- to remain connected
+        #     ALSO you need full fledged object of Edge -- to remain connected
         #     on one side (surrounding graph knowledge) when replacing edges
         #     connect-point to another node
         self._neighbors[uid1].add(uid2)
@@ -159,13 +205,14 @@ class ProxyGraph(object):
     def __init__(self):
         self._g = IdGraph()
 
-    def get_node(self):
-        return INode(self._g)
+    def get_node(self, uid):
+        return INode(self._g, uid)
 
 
 # Wrapper classes extend base container by shortcuts
 #   E.G. add_node_connected_to(node, [neighbour_nodes])
 #     == add_node() + add_edges_from()
+# THINK: replace dedicated "root" concept by aquired "entry point for edge" from GraphNode
 class RootedGraph(ProxyGraph):
     def __init__(self, rootid=None, **kw):
         self._rootid = rootid
@@ -173,4 +220,4 @@ class RootedGraph(ProxyGraph):
 
     def getRoot(self):
         """ Construct node object with backref to graph """
-        return self.get_node(self.rootid)
+        return self.get_node(self._rootid)
