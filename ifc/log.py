444c022eae8b2c965bdd39281e773f1754539c46
Amerlyq <amerlyq@gmail.com>
2021-09-18 15:10:43 +0300

    [ifc] * chg log.py  +5/-4

--- ifc/log.py
+++ ifc/log.py
@@ -4,7 +4,7 @@
 # SPDX-License-Identifier: GPL-3.0-only
 #
 __all__ = [
-    'getLogger',
+    "getLogger",
 ]
 
 import logging
@@ -14,11 +14,12 @@ import sys
 def getLogger(cmpt=None):
     if cmpt is None:
         # CHECK: in compiled Python code "inspect.currentframe()" don't work
+        # ALT:TRY: log the calling function name by overriding logger.findCaller in a custom logger class.
         caller = sys._getframe(1).f_back
         func = caller.f_code.co_name
-        mod = caller.f_globals['__name__']
-        cmpt = mod.partition('.')[2].partition('.')[0]
-        cmpt = cmpt if cmpt else 'root'
+        mod = caller.f_globals["__name__"]
+        cmpt = mod.partition(".")[2].partition(".")[0]
+        cmpt = cmpt if cmpt else "root"
 
     ## OBSOLETE: logging.Formatter has %(process:d) %(thread:d)
     # pid = os.getpid()
