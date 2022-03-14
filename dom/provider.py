4cb8d4c42a358fe7a402b9f04cc5afe7401f0cde
Amerlyq <amerlyq@gmail.com>
2022-03-14 08:56:46 +0200

    [miur] * chg provider.py  +34/-1

--- dom/provider.py
+++ dom/provider.py
@@ -1,3 +1,5 @@
+from subprocess import PIPE, Popen
+
 from just.iji.shell import runlines
 
 
@@ -18,7 +20,38 @@ class DataProvider:
         self.refresh()
 
     def refresh(self) -> None:
-        self._items = list(map(Item, runlines("pacman -Qtq")))
+        # self._items = list(map(Item, runlines("pacman -Qtq")))
+        self._items = []
+        # TODO: lazy parse -- store whole output but parse only during scrolling
+        with Popen("pacman -Qti".split(), stdout=PIPE, bufsize=1, text=True) as proc:
+            i = 0
+            nm = None
+            pk = None
+            pv = ""
+            # for i, l in enumerate(proc.stdout.readlines()):
+            assert proc.stdout
+            while l := proc.stdout.readline():
+                l = l.rstrip()
+                if not l and nm:
+                    x = Item(nm)
+                    nm = None
+                    self._items.append(x)
+                elif l.startswith(" "):
+                    pv += "\n" + l.lstrip()
+                else:
+                    if pk == "Name":
+                        nm = pv
+                    try:
+                        pk, pv = l.split(" : ", 1)
+                    except ValueError as exc:
+                        raise ValueError(i, l) from exc
+                    else:
+                        pk = pk.strip()
+                        pv = pv.strip()
+                i += 1
+            if nm:
+                x = Item(nm)
+                self._items.append(x)
 
     def __len__(self) -> int:
         # FUTURE: ret "+Inf" to indicate that it's unbounded stream -- for easier comparisons
