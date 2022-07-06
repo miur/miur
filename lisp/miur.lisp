b3104f572847cf9715cf3beec134fecee0199db6
Amerlyq <amerlyq+git@gmail.com>
2022-07-07 02:09:11 +0300

    [lisp] * chg 2 files  +16/-44

--- lisp/miur.lisp
+++ lisp/miur.lisp
@@ -6,40 +6,12 @@
 ;;
 (in-package #:miur)
 
-(defstruct wmstate
-  display
-  screen
-  root)
-
-(defparameter *mywm* (make-wmstate))
-(defparameter *quit* nil)
-
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Utils
-(defmacro with-gensyms (syms &body body)
-  `(let ,(mapcar #'(lambda (s) `(,s (gensym))) syms)
-     ,@body))
-
-(defmacro defrestart (name)
-  (let ((fname (intern (concatenate 'string "RESTART-" (symbol-name name)))))
-    (with-gensyms (c restart)
-      `(defun ,fname (,c)
-         (let ((,restart (find-restart ',name)))
-           (when ,restart
-             (invoke-restart ,restart ,c)))))))
-
-(defrestart window-error)
-
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Launch
-;; INFO:
-;; Common Lisp: uiop:run-program outputs but uiop:launch-program does not - Stack Overflow ⌇⡞⣔⠆⡤
-;;   https://stackoverflow.com/questions/57720432/common-lisp-uioprun-program-outputs-but-uioplaunch-program-does-not
 
 (defun ps ()
-  (let ((output (uiop:run-program '("ls" "-l") :output :string)))
+  (let ((output (uiop:run-program '("ls" "-l" "/data") :output :string)))
     (loop for line in (rest (cl-ppcre:split "(\\n+)" output))
           collect (cl-ppcre:split "(\\s\\s+)" line))))
 
@@ -47,27 +19,15 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Main
 
-(defun main (mywm) ;&key (myvar 2000))
-  (declare (ignore mywm))
-  ; (format t "~A~%" myvar)
+(defun main ()
   (print "=main")
   (finish-output nil)
   (unwind-protect
        (progn
          (print "=loop")
+         (print (ps))
          )
     (print "=cleanup"))
-  ;; (print myvar)
   (finish-output nil))
 
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;;; Run
-(eval-when (:execute)
-  (progn
-    (print "-----")
-    (print "=start")
-    (finish-output nil)
-    (main *mywm*)
-    (print "=end")
-    (finish-output nil)))
+; (main)
