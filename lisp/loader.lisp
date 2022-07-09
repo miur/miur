ad27dbdb3296bbca19642711d643aa03e50a4631
Amerlyq <amerlyq+git@gmail.com>
2022-07-09 21:45:03 +0300

    [lisp] * chg loader.lisp  +16/-5

--- lisp/loader.lisp
+++ lisp/loader.lisp
@@ -4,7 +4,7 @@
 (require "asdf")
 
 (eval-when (:compile-toplevel :execute :load-toplevel)
-  (ql:quickload '(:croatoan :swank)))
+  (ql:quickload '(:croatoan)))
 
 ; FIXME: directly load single system inof searching whole project tree
 (setf ql:*local-project-directories* (list (uiop/os:getcwd)))
@@ -15,12 +15,23 @@
 ; (push '*default-pathname-defaults* asdf:*central-registry*)
 ; (asdf:load-system :miur)
 
-(in-package :miur)
+; (in-package :miur)
+; *package*
 
-*package*
+;;; Vanilla SWANK
+; (ql:quickload '(:croatoan :swank)))
+; (require :swank)
+; (swank:create-server :port 4005 :dont-close t)
 
-(require :swank)
-(swank:create-server :port 4005 :dont-close t)
+
+;;; OR:NEED: /@/plugins/nvim/all/vlime/lisp -> ~/quicklisp/local-projects/lisp
+; (ql:quickload :vlime)
+; (vlime:main)
+; (vlime:main :interface #(127 0 0 1)
+;             :port 7002
+;             :backend :vlime-usocket)
+; BAD: blocking, single-thread NEED: multithread
+(load "/@/plugins/nvim/all/vlime/lisp/start-vlime.lisp")
 
 
 ; (eval-when (:execute)
