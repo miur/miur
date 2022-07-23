c35c5e3cc3027375392f066cbd992f0b45c356df
Amerlyq <amerlyq+git@gmail.com>
2022-07-24 02:44:47 +0300

    [lisp] * chg miur.lisp  +190/-49

--- lisp/miur.lisp
+++ lisp/miur.lisp
@@ -23,7 +23,7 @@
 (defun runhere (&rest args)
   (uiop:run-program args :force-shell nil :directory (uiop:getcwd) :output :string))
 
-(defun grep (x)
+(defun rungrep (x)
   (sort (str:lines (runhere "re" x)) 'string-lessp))
 
 ;; DEP: vim-fetch -- to be able to directly open spec "/path/to/file:15:20:"
@@ -37,7 +37,7 @@
 
 (defun runeditor ()
   (shell-out *scr* nil *editor* "--"
-             (filespec (item-text (undercursor)))))
+             (filespec (item-text (sw-curitem *sw*)))))
 
 
 ;; FAIL: stdin redir total trash -- try using temp files in /run
@@ -53,13 +53,14 @@
                      :direction :output
                      :if-exists :supersede
                      :if-does-not-exist :create)
-    (dolist (line (mapcar 'item-text (dbslice)))
+    (dolist (line (mapcar 'item-text (coerce (sw-visible *sw*) 'list)))
       (write-line line fd)))
     (shell-out *scr* nil *editor* "-c" "setl bt=nofile nowrap efm=%f:%l:%c:%m | cbuffer"
                "--" tmp)))
 
 
 ;; DEBUG: (myinput)
+#+(or)
 (defun myinput ()
   (with-open-file (file #P"miur.asd")
     (loop for i from 0
@@ -71,13 +72,19 @@
           ; collect (str:join ": " '((write-to-string i) line))
           )))
 
+(defun myinput ()
+  (rungrep ""))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; DOM
 
+; TODO: lazy access/query to all [other] metadata on-demand
 (defclass item ()
-  ((text :initarg :text :reader item-text)
-   ))
+  ((text :reader item-text
+         :initarg :text
+         :type string
+         :documentation "Text to show in widgets")
+   ) (:documentation "Single item with all related metadata combined"))
 
 (defun make-item (text)
   (make-instance 'item :text text))
@@ -87,54 +94,179 @@
       :key :raw))
 
 
-; (defparameter *dom* (mapcar 'make-item (myinput)))
-(defparameter *dom* (mapcar 'make-item (grep "")))
+(defclass snapshot ()
+  ((items :reader snapshot-items
+          :initarg :items
+          :type '(array item)
+          :documentation "Persistent copies of transient items")
+   ) (:documentation "Cached Snapshot of query results"))
 
-; TEMP: caching database for filesystem -- preload requested info only on-demand
-(defun lazydatabase ()
-  *dom*)
+; TODO: apply all sort/filter actions first -- only then slice it"
+(defmethod snapshot-slice ((x snapshot) &optional (beg 0) (end nil))
+  "Vertical query -- to preview necessary part of the list"
+  (subseq (snapshot-items) beg end))
 
-(defun dbslice ()
-  "Horizontal query to get current 'folder' view of chosen elements"
-  (lazydatabase))
 
-; HACK: both dbsclice and viewport can be combined into single direct query to DB
+; TEMP: lazy caching database for filesystem
+; FIXME: preload requested info only on-demand
+; CHG: data -> triples
+(defclass db ()
+  ((data :reader db-all
+         :initarg :data
+         ; :initform nil
+         :type '(array item)
+         :documentation "Convert all db data to item list")
+   ) (:documentation "Cached Knowledge Database"))
+
+(defun make-db (lines)
+  (let* ((xs (mapcar 'make-item lines))
+         (arr (make-array (length xs)
+                          :element-type 'item
+                          :initial-contents xs
+                          :adjustable t)))
+    (make-instance 'db :data arr)))
+
+; HACK: both "query" and "slice" can be combined into single direct query to DB
 ;   << this would allow us to work with infinite lists like "chats", etc.
-(defun viewport (beg end)
-  "Vertical query -- to preview necessary part of the list
-    BUT: only after all filters were applied"
-  (subseq (dbslice) beg end))
+; TODO: accept params to filter db list e.g. folder-cwd or grep-prefix
+; SRC: https://stackoverflow.com/questions/9549568/common-lisp-convert-between-lists-and-arrays
+(defmethod db-query ((x db))
+  "Horizontal query to get current 'folder' snapshot view of chosen elements"
+  (let ((xs (db-all x)))
+    (make-array (length xs)
+                :element-type 'item
+                ; OR: :displaced-to xs
+                :initial-contents xs
+                :adjustable nil)))
 
-(defun scrollwidget ()
-  "Scroll widget"
-  (viewport 0 8))
 
-(defun undercursor ()
-  (nth 3 (scrollwidget)))
+(defclass scrollwidget ()
+  ((provider :reader sw-all
+             :initarg :provider
+             :type snapshot
+             :documentation "Snapshot items")
+  (offset :reader sw-offset
+          :initarg :offset
+          :initform 0
+          :type integer
+          :documentation "Viewport window beginning")
+  (limit :reader sw-limit
+         :initarg :limit
+         :type integer
+         :documentation "Viewport window length")
+  (curabs :reader sw-curabs
+          :initarg :curabs
+          :initform 0
+          :type integer
+          :documentation "Cursor current absolute position")
+   ) (:documentation "Scroll widget"))
+
+(defun make-scrollwidget (snap height)
+  (make-instance 'scrollwidget :provider snap :limit height))
+
+(defmethod sw-currel ((x scrollwidget))
+  "Cursor position relative to viewport window offset"
+  (- (sw-curabs x) (sw-offset x)))
+
+(defmethod sw-curitem ((x scrollwidget))
+  "Get single item under current cursor"
+  (aref (sw-all x) (sw-curabs x)))
+
+(defmethod sw-visible ((x scrollwidget))
+  "Visible items inside viewport window"
+  (subseq (sw-all x) (sw-offset x) (+ (sw-offset x) (sw-limit x))))
+
+(defmethod (setf sw-limit) (v (x scrollwidget))
+  "Set viewport height"
+  (when (or (< v 0) (> v (length (slot-value x 'provider))))
+    (error "Limit is out of bounds"))
+  (setf (slot-value x 'limit) v))
+
+(defmethod (setf sw-currel) (v (x scrollwidget))
+  "Set cursor position relative to viewport"
+  (setf (sw-curabs x) (+ v (slot-value x 'offset))))
+
+(defmethod (setf sw-curabs) (v (x scrollwidget))
+  "Set cursor absolute position relative to snapshot -- and move window"
+  (let* ((keep 3)
+         (beg 0)
+         (bot (+ beg keep))
+         (off (sw-offset x))
+         (low (+ off keep))
+         (lim (sw-limit x))
+         ; (upp (1- (+ off lim (- keep)))
+         (upp (+ off lim (- keep)))
+         (end (1- (length (sw-all x))))
+         (top (- end keep)))
+    (multiple-value-bind (wnd abs)
+        (cond
+          ; HACK: jump to last element
+          ((null v) (values (- end lim) end))
+          ((< v beg) (values beg beg))
+          ((< v bot) (values beg v))
+          ((< v low) (values (- v keep) v))
+          ((< v upp) (values off v))
+          ; ((< v top) (values (1- (+ v keep (- lim))) v))
+          ((< v top) (values (+ v keep (- lim)) v))
+          ((< v end) (values (- end lim) v))
+          (t (values (- end lim) end)))
+      ; MAYBE:ALT: multiple-value-setq
+      (unless (= off wnd) (setf (slot-value x 'offset) wnd))
+      (setf (slot-value x 'curabs) abs)
+      )))
+
+(defmethod (setf sw-offset) (v (x scrollwidget))
+  "Set window absolute position relative to snapshot -- and move cursor"
+  (let* ((keep 3)
+         (bot 0)
+         (cur (sw-curabs x))
+         (off (sw-offset x))
+         (low (+ off keep))
+         (lim (sw-limit x))
+         (upp (+ off lim (- keep)))
+         (end (1- (length (sw-all x))))
+         (top (- end lim)))
+    (multiple-value-bind (wnd abs)
+        (cond
+          ; HACK: jump to last element
+          ((null v) (values (- end lim) (max low (min end cur))))
+          ((< v bot) (values bot (max bot (min upp cur))))
+          ((< v top) (values v (max low (min upp cur))))
+          (t (values (- end lim) (max low (min end cur)))))
+      ; MAYBE:ALT: multiple-value-setq
+      (unless (= off wnd) (setf (slot-value x 'offset) wnd))
+      (setf (slot-value x 'curabs) abs)
+      )))
+
+(defparameter *db* (make-db (myinput)))
+(defparameter *sw* (make-scrollwidget (db-query *db*) 10))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; View
+
+(defun drawline (scr i x ww cur off)
+  (let* ((text (str:shorten ww (item-text x)))
+         (attr (if (= i cur) '(:reverse :bold) '(:normal)))
+         (clr (if (eql :key (item-type x)) :terminal :teal))
+         (pfx (format nil "~02d| ~02d" i (+ off i))))
+    ;; DEBUG:
+    ; (nc:submit (croatoan:add-string miur::*scr* "Hey!"))
+    ; (nc:submit (format *swank-output* "Hellou!~%"))
+    (nc:add-string scr pfx :x 0 :y i :fgcolor :lime :bgcolor :terminal)
+    (nc:add-string scr text :x 7 :y i :attributes attr :fgcolor clr :bgcolor :terminal)
+    ))
 
 ;; DEBUG: (nc:submit (draw *scr*))
 (defun draw (scr)
   ; BAD:(not implemented): (nc:window-size *scr*)
   (let ((ww (nc:width scr))
-        (hh (nc:height scr))
-        (beg 0)
-        (cur 3))
+        ; (hh (nc:height scr))
+        (off (sw-offset *sw*))
+        (cur (sw-currel *sw*)))
     (nc:clear scr)
     (nc:move scr 0 2)
-    (loop for i from 0 to (+ hh -1)
-          for x in (scrollwidget)
-          do (let* ((attr (if (= i cur) '(:reverse :bold) '(:normal)))
-                    (clr (if (eql :key (item-type x)) :terminal :teal))
-                    (pfx (format nil "~02d| ~02d" i (+ beg i))))
-               ;; DEBUG:
-               ; (nc:submit (croatoan:add-string miur::*scr* "Hey!"))
-               ; (nc:submit (format *swank-output* "Hellou!~%"))
-               (nc:add-string scr pfx :x 0 :y i :fgcolor :lime :bgcolor :terminal)
-               (nc:add-string scr (item-text x) :x 7 :y i :attributes attr :fgcolor clr :bgcolor :terminal)
-               ))
+    (loop for x across (sw-visible *sw*) and i from 0
+          do (drawline scr i x ww cur off))
     (nc:move scr cur 0)
     (nc:refresh scr)))
 
@@ -150,7 +282,6 @@
   (ncurses:endwin)  ; restore original tty modes
   (format *swank-output* "Shelling out...~%")
   (unwind-protect
-    ; (grep "xxx")
     ; ALT:(:interactive): *swank-output* | uiop/stream:*output*
     (uiop:run-program cmdline
                       :force-shell nil
@@ -173,17 +304,26 @@
 
 ;; DEBUG: (nc:submit (keybinds *scr*))
 (defun keybinds (scr)
-  (nc:bind scr " " (lambda (w ev) (draw scr) (nc:refresh scr)))
+  (nc:bind scr "^D" 'sb-ext:quit)
+  (nc:bind scr #\q 'nc:exit-event-loop)
   (nc:bind scr #\c (lambda (w ev) (nc:clear scr) (nc:refresh scr)))
+  (nc:bind scr " " (lambda (w ev) (draw scr)))
   (nc:bind scr #\s (lambda (w ev) (runshell)))
   (nc:bind scr #\Newline (lambda (w ev) (runeditor)))
   (nc:bind scr #\o (lambda (w ev) (runquickfix)))
-  (nc:bind scr #\q 'nc:exit-event-loop)
-  (nc:bind scr "^D" 'sb-ext:quit)
+
+  (nc:bind scr #\J (lambda (w ev) (incf (sw-offset *sw*)  1) (draw scr)))
+  (nc:bind scr #\K (lambda (w ev) (incf (sw-offset *sw*) -1) (draw scr)))
+  (nc:bind scr #\j (lambda (w ev) (incf (sw-currel *sw*)  1) (draw scr)))
+  (nc:bind scr #\k (lambda (w ev) (incf (sw-currel *sw*) -1) (draw scr)))
+  (nc:bind scr #\g (lambda (w ev) (setf (sw-curabs *sw*)  0) (draw scr)))
+  (nc:bind scr #\G (lambda (w ev) (setf (sw-curabs *sw*) nil) (draw scr)))
+
   (nc:bind scr :resize (lambda (w ev)
-      (nc:add-string scr (format nil "Resize: WxH=~a ~a    "
-                                 (nc:width w) (nc:height w)) :x 80 :y 1)
-      (nc:refresh scr)))
+                         (setf (sw-limit *sw*) (nc:height w))
+                         (nc:add-string scr (format nil "Resize: WxH=~a ~a    "
+                                                    (nc:width w) (nc:height w)) :x 80 :y 1)
+                         (nc:refresh scr)))
   (nc:bind scr t #'drawkeyinfo))
 
 
@@ -201,6 +341,7 @@
                        :input-blocking 100
                        :bind-debugger-hook nil)
     (setf *scr* scr)
+    (setf (sw-limit *sw*) (nc:height scr))
     ; (setf (nc:attributes scr) '(:bold))
     ; (setf (nc:color-pair scr) '(:yellow :red))
     (nc:clear scr)
