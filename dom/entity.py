9bc4632933a87e81fc3be73170b0f99b386823fa
Amerlyq <amerlyq+git@gmail.com>
2024-03-07 05:20:34 +0200

    [miur] * chg 2 files  +168/-8

--- dom/entity.py
+++ dom/entity.py
@@ -66,14 +66,19 @@ class FileEntity(InodeEntity):
                     f.seek(i * 16, 0)
                     yield HexBlobEntity(f.read(16))  # .hex()
 
-    # WARN: Entity should support multitude of own obj actions
-    #   *and* its parent actions e.g. "get_stats", "get_lines"
-    ## .elf
-    # def get_sections|segments|symbols(self):
-    #     pass
-    ## .json
-    # def get_json_dict|list|keys|values(self):
-    #     pass
+
+# DECI: name after "outer container" (=DirectoryListing) ?
+#   OR after "common ancestor of elements" (=InodeListing)
+#   BUT: how to re-iterate over *generated* listing -- should we cache items inside XXXListing ?
+#     BUT: for very long folders/IRC -- we shouldn't cache whole lists (more RAM, more latency)
+#       BUT: lists usually need to be sorted -- it's hard to not get whole list first to sort once
+#         ~~ though, IRC is ts-appended, and file listing can be insert-sorted...
+class InodeListing(object):
+    def __init__(self, path: Path) -> None:
+        self._path = path
+
+    def get_stats(self) -> Iterable[StatEntity]:
+        yield from StatEntity(os.stat(self._path))
 
 
 class DirectoryEntity(InodeEntity):
@@ -90,3 +95,32 @@ class DirectoryEntity(InodeEntity):
         yield ShowActionEntity("FSListWidget", "FSView", "get_listing")
         yield ShowActionEntity("KV(keyval)ListWidget", "AttrView", "get_stats")
         yield ShowActionEntity("VirtualListWidget", "EntityView", "get_actions")
+
+
+# WARN: Entity should support multitude of own obj actions
+#   *and* its parent actions e.g. "get_stats", "get_lines"
+# BAD: backed to file Elf may have dual representation in RAM (for running process)
+class ElfEntity(FileEntity):
+    preferred = "lines"
+
+    # def get_sections|segments|symbols(self):
+    #     pass
+
+
+# BAD: can be Memory-based (STDIN), Stream-based (HTTP), or DOM-based (lang native structs)
+#   ALSO: can at any moment become File-backed or DB-backed (for persistence) and sync changes into storage
+#   THINK: how to use dynamic composition OR mix-ins to approximate everything ?
+#     IDEA: assign list of "traits" to each Entity -- like supported API sets / generic procedures
+class JsonEntity(FileEntity):
+    preferred = "highlights/sketch/majorants"
+
+    # def get_json_dict|list|keys|values(self):
+    #     pass
+
+
+# ALSO: streams/cmpts/alt-views for same identity
+#   .jpg -- FileContent, FileInode, FileMetadata, FilePreview, FileHash
+
+
+## !WF! Everything boils down to how API will be used by Widget
+##   -- i.e. how to efficiently access those classes above
