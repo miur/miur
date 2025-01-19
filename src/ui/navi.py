5a1aae55971a42a2d31bf52fc797c06f32385d37
Amerlyq <amerlyq+git@gmail.com>
2025-01-19 06:46:03 +0200

    adaptive layout per some rnd width

--- src/ui/navi.py
+++ src/ui/navi.py
@@ -47,6 +47,11 @@ class Panel:
         # FIXME: search recursively
         return next(p for p in self._split if p.name == name)
 
+    def __contains__(self, nm: str) -> bool:
+        if self.name == nm:
+            return True
+        return any(nm in p for p in self._split)
+
     def named_rects(self) -> Iterable[tuple[str, Rect]]:
         for p in self._split:
             if nm := p.name:
@@ -69,20 +74,48 @@ class Panel:
             vx += w
 
 
-# TODO:(`AdaptiveLayout): change/hide based on total window size
+# NOTE:(`AdaptiveLayout): change/hide based on total window size
 #   DFL:(prio): browser:0 -> preview:1 -> parent:2 [-> pparent:3]
 # TODO: toggle/limit linewrap + content-awareness
 # TODO: header/footer hide/xfm
-def pick_adaptive_layout_cfg(ww: int, old: Panel) -> Panel:
-    if ww < 50:
-        raise NotImplementedError(ww)
-    if old.name == "navi_main":
+def pick_adaptive_layout_cfg(rect: Rect, old: Panel) -> Panel:
+    if rect.w < 30:
+        if old.name == "navi_vlst":
+            return old
+        browse = Panel("browse", [Panel("tab0")])
+        return Panel("navi_vlst", [browse])
+
+    if rect.w < 45:
+        if old.name == "navi_pv0":
+            return old
+        browse = Panel("browse", [Panel("tab0")])
+        preview = Panel("preview", [Panel("pv0")], w=12)
+        return Panel("navi_pv0", [browse, preview])
+
+    if rect.w < 60:
+        if old.name == "navi_miller0":
+            return old
+        prevloci = Panel("prevloci", [Panel("prev")], w=8)
+        browse = Panel("browse", [Panel("tab0")])
+        preview = Panel("preview", [Panel("pv0")], w=12)
+        return Panel("navi_miller0", [prevloci, browse, preview])
+
+    if rect.w < 70:
+        if old.name == "navi_miller1":
+            return old
+        prevloci = Panel("prevloci", [Panel("prev")], w=16)
+        browse = Panel("browse", [Panel("tab0")], w=0.5)
+        preview = Panel("preview", [Panel("pv0")])
+        return Panel("navi_miller1", [prevloci, browse, preview])
+
+    if old.name == "navi_miller2":
         return old
     prevloci = Panel("prevloci", [Panel("pprev", w=0.4), Panel("prev")], w=22)
+    browse = Panel("browse", [Panel("tab0")], w=0.5)
     # interp = PanelView(ratio=(0,))
     preview = Panel("preview", [Panel("pv0", w=0.7), Panel("pv1")])
     # TODO:ALSO: return linewrap/header/footer cfg overrides
-    return Panel("navi_main", [prevloci, Panel("browse", w=0.5), preview])
+    return Panel("navi_miller2", [prevloci, browse, preview])
 
 
 # SPLIT:(`NaviLayout): to cvt {(ww,wh) -> [panel].rect}, adapt based on size and toggle visibility
@@ -101,23 +134,27 @@ class NaviWidget:
 
     ## ALT:(rect/origin): do C.move(y,x) b4 .redraw(), and remember getyx() inside each .redraw()
     def resize(self, vh: int, vw: int, orig_yx: tuple[int, int] = (0, 0)) -> None:
-        self._layout = pick_adaptive_layout_cfg(vw, self._layout)
-        self._layout.resize(Rect(vw, vh, x=orig_yx[1], y=orig_yx[0]))
+        rect = Rect(vw, vh, x=orig_yx[1], y=orig_yx[0])
+        self._layout = pick_adaptive_layout_cfg(rect, self._layout)
+        self._layout.resize(rect)
         # log.debug(f"{self._layout=}")  # <TEMP:DEBUG
-        # WHY: adaptive layout on bigger window may need more preview nodes
-        self._update_preview()
+        if "preview" in self._layout:
+            # WHY: adaptive layout on bigger window may need more preview nodes
+            self._update_preview()
+            self._resize_cached_preview()
         self._resize_cached_hist_browse()
-        self._resize_cached_preview()
 
     def cursor_jump_to(self, idx: int) -> None:
         self._view._wdg.focus_on(idx)  # pylint:disable=protected-access
-        self._update_preview()
-        self._resize_cached_preview()
+        if "preview" in self._layout:
+            self._update_preview()
+            self._resize_cached_preview()
 
     def cursor_step_by(self, steps: int) -> None:
         self._view._wdg.step_by(steps)  # pylint:disable=protected-access
-        self._update_preview()
-        self._resize_cached_preview()
+        if "preview" in self._layout:
+            self._update_preview()
+            self._resize_cached_preview()
 
     def view_go_into(self) -> None:
         # pylint:disable=protected-access
@@ -129,21 +166,24 @@ class NaviWidget:
 
     def view_jump_to(self, nent: Golden) -> None:
         self._hist.jump_to(nent)
-        self._update_preview()
+        if "preview" in self._layout:
+            self._update_preview()
+            self._resize_cached_preview()
         self._resize_cached_hist_browse()
-        self._resize_cached_preview()
 
     def view_go_back(self) -> None:
         self._hist.go_back()
-        # WHY: after previous jump_to() we may return to disjoint parent with different preview()
-        self._update_preview()
+        if "preview" in self._layout:
+            # WHY: after previous jump_to() we may return to disjoint parent with different preview()
+            self._update_preview()
+            self._resize_cached_preview()
         # WHY: forced resize() old/cached wdg, as window may had resized from then.
         self._resize_cached_hist_browse()
-        self._resize_cached_preview()
 
     def _update_preview(self) -> None:
         # pylint:disable=protected-access
         wdg = self._hist.focused_view._wdg
+        # [_] RFC: isolate same ALG of traversing list of `Panels for _resize_cached*(), etc.
         for _ in self._layout["preview"]:
             if not wdg._lst:
                 break
@@ -193,7 +233,9 @@ class NaviWidget:
                 prev._wdg.resize(pr.h, pr.w, origin=(pr.y, pr.x))
                 pr = None
 
-        for r in (self._layout["browse"].rect,):
+        # MAYBE: extend browse= to whole hist/preview when hist=none or preview=none
+        for p in self._layout["browse"]:
+            r = p.rect
             self._view._wdg.resize(r.h, r.w, origin=(r.y, r.x))
 
     def redraw(self, stdscr: C.window) -> tuple[int, int]:
@@ -203,19 +245,20 @@ class NaviWidget:
             if prev := self._hist.get_relative(i):
                 prev._wdg.redraw(stdscr, numcol=False)
 
-        wdg = self._hist.focused_view._wdg
-        for _ in self._layout["preview"]:
-            if not wdg._lst:
-                break
-            peek = self._pool.get(wdg.focused_item._ent)
-            if not peek:
-                break  # COS: consequent previews are depending on previous ones
-            wdg = peek._wdg
-            wdg.redraw(stdscr, numcol=False)
+        if "preview" in self._layout:
+            wdg = self._hist.focused_view._wdg
+            for _ in self._layout["preview"]:
+                if not wdg._lst:
+                    break
+                peek = self._pool.get(wdg.focused_item._ent)
+                if not peek:
+                    break  # COS: consequent previews are depending on previous ones
+                wdg = peek._wdg
+                wdg.redraw(stdscr, numcol=False)
 
         # NOTE: draw main Browse column very last to always be on top
         curyx = (0, 0)
-        for _ in (self._layout["browse"],):
+        for _ in self._layout["browse"]:
             curyx = self._view._wdg.redraw(stdscr, numcol=True)
         # TODO: return curyx from focused panel inof the last one on the right
         return curyx
